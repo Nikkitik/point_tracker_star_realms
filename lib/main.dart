6e0d33c419fea749ba7904b6af91edfed28ff929
nikita.romantsov <nikita.romantsov@sravni.ru>
2021-12-10 01:01:39 +0300

    удалил неиспользуемый код

--- lib/main.dart
+++ lib/main.dart
@@ -11,47 +11,11 @@ class MyPointer extends StatefulWidget {
 }
 
 class _MyPointerState extends State<MyPointer> {
-  //Блок 'Жизни'
-  int _healthCount = 50;
-  String _healthTitle = 'Жизни';
-
-  //Блок 'Игроки'
-  int _userCount = 2;
-  String _userTitle = 'Игроки';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Star Realms Pointer',
       home: SettingsScreen(),
-        //     Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-        //   // Expanded(
-        //   //   child: UserListWidget(),
-        //   // ),
-        //   Row(children: [
-        //     Expanded(
-        //       child: StartCardWidget(
-        //         value: _healthCount,
-        //         title: _healthTitle,
-        //         needProvider: false,
-        //       ),
-        //     ),
-        //     Expanded(
-        //       child: StartCardWidget(
-        //         value: _userCount,
-        //         title: _userTitle,
-        //         needProvider: true,
-        //       ),
-        //     ),
-        //   ]),
-        //   Padding(
-        //     padding: const EdgeInsets.all(4.0),
-        //     child: ElevatedButton(
-        //       onPressed: () {},
-        //       child: Text('Начать игру'),
-        //     ),
-        //   ),
-        // ]),
     );
   }
 }
